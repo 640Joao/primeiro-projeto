fd4714ebed6393a8717d78678912f723405b3f37
640Joao <joanegreiros15@gmail.com>
2026-06-12 21:25:33 -0300

    outro commit

---
+++ primeiro-programa.py
@@ -0,0 +1,7 @@
+nome = "João Elias"
+sobrenome = "Negrerios de Souza Matos"
+idade = 29
+altura = 1.74
+maior_de_idade = True
+print(f"Olá", nome, sobrenome, "você tem ", idade, "anos, você é maior de idade?", maior_de_idade)
+print(f"Olá {nome} {sobrenome} você tem idade {idade} anos, você é maior de idade? {maior_de_idade}")

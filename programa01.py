62394b1597df5e30d2f39f931fc3462894b90a6a
640Joao <joanegreiros15@gmail.com>
2026-06-15 21:22:06 -0300

    novo commit

---
+++ programa01.py
@@ -0,0 +1,5 @@
+nome  = input(print("Digite seu nome:"))
+print(f"Olá {nome} seja bem vindo")
+
+idade = input(print("Prazer em te conhecer, digite sua idade"))
+
---
+++ programa03.py
@@ -0,0 +1,14 @@
+#calculadora python
+
+num1 = int(input("Digite o primeiro número: "))
+
+num2 = int(input("Digite o segundo número: "))
+
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+potencia = pow(num1, num2)
+
+print(f"O valor da soma entre {num1} + {num2} = {soma}: ")
+
